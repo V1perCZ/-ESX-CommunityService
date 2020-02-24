40baf28be3deeb63d7a26f4bf474f809eaeb6b98
Lukáš Müller <61428713+V1perCZ@users.noreply.github.com>
2020-02-24 20:48:35 +0100

    Add files via upload

---
+++ ESX_CommunityService-master/locales/en.lua
@@ -0,0 +1,16 @@
+Locales ['en'] = {
+	['judge']              	= 'SOUDCE',
+	['escape_attempt']     	= 'Nemuzete utect. Vase komunitni sluzba byla rozsirena.',
+	['remaining_msg']      	= 'Mas ~b~%s~s~ nez dokoncite sluzbu, dokoncete vice akci.',
+	['comserv_msg']       	= '%s byl odsouzen na %s mesice komunitni sluzby.',
+	['comserv_finished']  	= '%s dokoncil komunitni sluzbu!',
+	['press_to_start']      = 'Pres ~INPUT_CONTEXT~ zacnes pracovat.',
+	['system_msn']          = '^1SYSTEM',
+	['invalid_player_id_or_actions']      = 'Neplatne ID hrace nebo pocet akci.',
+	['insufficient_permissions']      = 'Nedostatecna opravneni.',
+	['give_player_community']      = 'Poskytnete komunitni sluzbu hrace',
+	['target_id']      = 'cilove ID',
+	['action_count_suggested']      = 'pocet akci [navrzeno: 10-40]',
+	['invalid_player_id']      = 'Neplatne ID hrace!',
+	['unjail_people']      = 'Odpojte lidi z vezení'
+}
